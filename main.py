1e246e6a7079867207d2ed87e5529f9b713aa5a0
justinryu88 <66536387+justinryu88@users.noreply.github.com>
2023-08-04 23:40:28 +0000

    added sds and formula to search page

--- main.py
+++ main.py
@@ -23,8 +23,18 @@ def search_files():
 
     service = build_from_document(discovery_data, credentials=creds)
 
+    # Map folder names to folder IDs and their respective page sizes
+    folders = {
+        'COA': ('1BTTgMBVhg8mG0dQ5H8e5KT-ltQUohS7B', 3),  # replace with your folder id and page size
+        'SDS': ('1jK9zt2wXqWLnpyQqrk1at0cbMJdXElpi', 5),  # replace with your folder id and page size
+        'Formula': ('1Ee9JZQnIx5JO-qFGIxwGY7q5uBKXzIzJ', 50),  # replace with your folder id and page size
+    }
+
+    # Get the selected folder from the form data, default to 'COA' if none is selected
+    selected_folder = request.args.get('folder', 'COA')
+
     # Call the Drive v3 API to list the files in the folder
-    folder_id = '1BTTgMBVhg8mG0dQ5H8e5KT-ltQUohS7B'
+    folder_id, page_size = folders[selected_folder]
     query = f"'{folder_id}' in parents"
 
     search_term = request.args.get('search_term')
@@ -33,10 +43,10 @@ def search_files():
 
     results = service.files().list(
         q=query,
-        pageSize=3, fields="nextPageToken, files(id, name)").execute()
+        pageSize=page_size, fields="nextPageToken, files(id, name)").execute()
     items = results.get('files', [])
 
-    return render_template('results.html', results=items, search_term=search_term)
+    return render_template('results.html', results=items, search_term=search_term, selected_folder=selected_folder)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
